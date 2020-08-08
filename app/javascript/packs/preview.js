2ebcf9dfefec1174b4f19dc53fd90f222e373ef4
shunichfukui <68207981+shunichfukui@users.noreply.github.com>
2020-08-08 19:40:39 +0900

    プレビュー機能

---
+++ app/javascript/packs/preview.js
@@ -0,0 +1,30 @@
+if (window.location.pathname === '/book/new') {
+  document.addEventListener('DOMContentLoaded', function(){
+    const ImageList = document.getElementById('image-list')
+
+   
+    const createImageHTML = (blob) => {
+      const imageElement = document.createElement('div')
+
+      
+      const blobImage = document.createElement('img')
+      blobImage.setAttribute('src', blob)
+    
+      imageElement.appendChild(blobImage)
+      ImageList.appendChild(imageElement)
+    }
+
+    document.getElementById('content_image').addEventListener('change', function(e){
+      
+      const imageContent = document.querySelector('img');
+      if (imageContent){
+        imageContent.remove();
+      };
+
+      const file = e.target.files[0];
+      const blob = window.URL.createObjectURL(file);
+
+      createImageHTML(blob)
+    });
+  });
+}

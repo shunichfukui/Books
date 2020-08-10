5a9966d408a9c118108a80d9aed62c0c1870c26a
shunichfukui <68207981+shunichfukui@users.noreply.github.com>
2020-08-10 10:18:43 +0900

    チャット作成

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,6 +2,7 @@ class Book < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :genre
   belongs_to :user
+  has_one :room, dependent: :destroy
   has_one_attached :image
 
   validates :content, presence: true, unless: :was_attached?

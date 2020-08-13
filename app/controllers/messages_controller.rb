959342ce99041a18320c7660340afa005a0a91b7
shunichfukui <68207981+shunichfukui@users.noreply.github.com>
2020-08-13 14:01:40 +0900

    保存

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,7 @@
 class MessagesController < ApplicationController
-  
+  def index
+    @room =Room.all
+  end
 
   def new
     @book = Book.find(params[:book_id])
--- app/models/room.rb
+++ app/models/room.rb
@@ -2,4 +2,6 @@ class Room < ApplicationRecord
   belongs_to :book, optional: true
   has_many :room_users
   has_many :users, through: :room_users
+  validates :field ,presence: true
+
 end

b4f3d571942cc549aa1092e52596862d0ea1b8e7
shunichfukui <68207981+shunichfukui@users.noreply.github.com>
2020-08-15 09:07:50 +0900

    単体テスト

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Message, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @message = FactoryBot.build(:message)
+  end
+  describe '商品出品機能' do
+    it "textが空だと登録できない" do
+      
+      @message.errors[:text]    
+    end
+  end
 end
--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Tag, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @tag = FactoryBot.build(:tag)
+  end
+  describe 'タグ機能' do
+    it "tag_nameが空だと登録できない" do
+      @tag.errors[:tag_name]    
+    end
+  end
 end

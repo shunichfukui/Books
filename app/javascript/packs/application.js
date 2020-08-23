e5a9349616b3223fa91e63babdc4b0491393ae01
shunichfukui <68207981+shunichfukui@users.noreply.github.com>
2020-08-23 13:00:57 +0900

    いいね

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,7 +4,6 @@
 // that code so it'll be compiled.
 
 require("@rails/ujs").start()
-// require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require('jquery')
@@ -13,8 +12,10 @@ require('./preview')
 require("../tag")
 
 require('../button')
+//= require rails-ujs 
 //= require jquery 
-//= require rails-ujs
+//= require jquery_ujs 
+//= require_tree .
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.

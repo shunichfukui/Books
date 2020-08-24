561b31cad788efe55da269a479e11e3628723b58
shunichfukui <68207981+shunichfukui@users.noreply.github.com>
2020-08-25 08:10:05 +0900

    修正

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,6 +4,7 @@
 // that code so it'll be compiled.
 
 require("@rails/ujs").start()
+// require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require('jquery')
@@ -11,8 +12,9 @@ require('../fadeIn')
 require('./preview')
 require("../tag")
 
-require('../button')
+
 //= require rails-ujs 
+//= require turbolinks
 //= require jquery 
 //= require jquery_ujs 
 //= require_tree .

003dcda204ac2ffc007bedb409a76372c3b7d3bd
shunichfukui <68207981+shunichfukui@users.noreply.github.com>
2020-08-30 06:43:06 +0900

    リファクタリング

--- spec/factories/tags.rb
+++
@@ -1,5 +0,0 @@
-FactoryBot.define do
-  factory :tag do
-    tag_name { '#本' }
-  end
-end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
     nickname { 'suzuki' }
-    email                 { 'example@cmail.com' }
+    email    { 'example@cmail.com' }
     password { 111111 }
     password_confirmation {password}
   end

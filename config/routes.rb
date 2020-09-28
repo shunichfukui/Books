dd2a87ef7a1c058a571cf98bd39dbd4b7d483d23
shunichfukui <68207981+shunichfukui@users.noreply.github.com>
2020-09-28 19:21:15 +0900

    amazon検索機能

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   root to: "books#index"
   post 'books/messages'
+  get 'books/amazon'
   resources :books do
     resources :messages,only: [:create, :new,:index,:show]
     resource :favorites, only: [:create, :destroy  ]
